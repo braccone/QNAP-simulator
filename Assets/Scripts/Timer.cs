e64328beef09ce7c91ecb1f33272c25ded848284
Braccone <brahimjaddi94@gmail.com>
2017-07-21 17:23:23 +0200

    aggiunta oggetto timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    float startTime;
+    // Use this for initialization
+    void Start()
+    {
+        startTime = Time.time;
+    }
+	// Update is called once per frame
+	void Update () {
+        float t = Time.time - startTime;
+        string seconds = (t % 60).ToString("f0");
+        Debug.Log(seconds);
+	}
+}
