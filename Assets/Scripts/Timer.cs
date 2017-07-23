3c3906f624594fba1ac85cf6113b261ea2c5670c
Braccone <brahimjaddi94@gmail.com>
2017-07-23 22:51:48 +0200

    Inserimento tutte le sezioni e ingressi per i 3 incroci

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -8,13 +8,14 @@ public class Timer : MonoBehaviour {
     void Start()
     {
         startTime = Time.time;
-        GameObject temp = GameObject.Find("Incrocio_1");
-        temp.transform.Find("ingresso_1").gameObject.GetComponent<ingresso>().CreateCar(4);
+        GameObject temp = GameObject.Find("Incrocio_3");
+        Debug.Log(temp.transform.Find("ingresso_2").gameObject.name);
+        temp.transform.Find("ingresso_2").gameObject.GetComponent<ingresso>().CreateCar(4);
     }
 	// Update is called once per frame
 	void Update () {
         float t = Time.time - startTime;
         string seconds = (t % 60).ToString("f0");
-        Debug.Log(seconds);
+        //Debug.Log(seconds);
 	}
 }
